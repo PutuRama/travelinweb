eb2266417b1d466bff7125bdec09fe8b3072fdaf
kadekdicka <kadekdika864@gmail.com>
2024-11-01 13:54:38 +0800

    web jadi satu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.karoselgambar-paket').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.karoselgambar-icon').slick({
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
