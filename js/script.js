4106e1fa49e0a257d493f2d58eb664eee13797d7
PutuRama <pratamputurama@gmail.com>
2024-11-01 16:17:59 +0800

    still no solution

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-$('.karoselgambar-paket').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+$(".karoselgambar-paket").slick({
+	arrows: false,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 5000,
 });
 
-$('.karoselgambar-icon').slick({
-    arrows: false,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+$(".karoselgambar-icon").slick({
+	arrows: false,
+	slidesToShow: 6,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 4000,
 });
