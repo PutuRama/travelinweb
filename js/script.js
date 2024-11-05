053c479cea43a08805a0261282afadbed66ed556
PutuRama <pratamputurama@gmail.com>
2024-11-05 13:36:44 +0800

    responsive dan finishing section paket tur

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 $(".karoselgambar-paket").slick({
-	arrows: false,
 	slidesToShow: 3,
 	slidesToScroll: 1,
+	infinite: true,
 	autoplay: true,
+	arrows: false,
 	autoplaySpeed: 5000,
+	speed: 300,
+	asNavFor: ".karoselarrow",
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+	],
+});
+
+$(".karoselarrow").slick({
+	speed: 300,
+	arrows: true,
+	infinite: true,
+	asNavFor: ".karoselgambar-paket",
 });
 
 $(".karoselgambar-icon").slick({
@@ -12,4 +37,25 @@ $(".karoselgambar-icon").slick({
 	slidesToScroll: 1,
 	autoplay: true,
 	autoplaySpeed: 4000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 349,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+	],
 });
