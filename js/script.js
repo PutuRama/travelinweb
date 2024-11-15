93adf57adb62469da6577d02f7543a9601fbf365
PutuRama <pratamputurama@gmail.com>
2024-11-15 13:18:03 +0800

    failed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(".hero__slick").slick({
+	autoplay: true,
+	autoplaySpeed: 3000,
 	infinite: true,
 	slidesToShow: 1,
 	slidesToScroll: 1,
+	speed: 3000,
 	arrows: true,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				arrows: false,
+			},
+		},
+	],
 });
