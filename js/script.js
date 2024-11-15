36172d197a6a0bbe78287edd28eab4fae02bbe3e
adiputraalit <adiputraalit00@gmail.com>
2024-11-15 13:30:01 +0800

    kolp

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ $('.autoplay').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+$('.partner-slick').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
